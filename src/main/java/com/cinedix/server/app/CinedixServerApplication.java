cc27a6b3d1d3a8ba3ef0045ca59a7f6c47b279c4
charly30000 <andresbuzon1@hotmail.com>
2021-04-21 20:27:28 +0200

    Inicio de repositorio

---
+++ src/main/java/com/cinedix/server/app/CinedixServerApplication.java
@@ -0,0 +1,13 @@
+package com.cinedix.server.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinedixServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinedixServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinedix/server/web/app/CinedixServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinedix.server.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinedixServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
