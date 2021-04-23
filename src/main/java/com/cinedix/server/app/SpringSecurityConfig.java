5f2d6613a46af23840b3fa9759c26812118df5de
charly30000 <andresbuzon1@hotmail.com>
2021-04-23 22:42:41 +0200

    Annadido pag error 403 y seguridad por roles utilizando anotaciones

--- src/main/java/com/cinedix/server/app/SpringSecurityConfig.java
+++ src/main/java/com/cinedix/server/app/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -11,6 +12,7 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -23,7 +25,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		.and()
 		.formLogin().loginPage("/login").permitAll()
 		.and()
-		.logout().permitAll();
+		.logout().permitAll()
+		.and()
+		.exceptionHandling().accessDeniedPage("/errors/error_403");
 	}
 
 	@Bean
@@ -39,6 +43,6 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		builder.inMemoryAuthentication()
 		.withUser(users.username("admin").password("12345").roles("ADMIN", "USER"))
-		.withUser(users.username("andres").password("12345").roles("ADMIN"));
+		.withUser(users.username("andres").password("12345").roles("USER"));
 	}
 }
--- src/main/java/com/cinedix/server/app/controllers/IndexController.java
+++ src/main/java/com/cinedix/server/app/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.cinedix.server.app.controllers;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+	@Secured("ROLE_ADMIN")
 	@GetMapping({"/", "/index"})
 	public String index(Model model) {
 		model.addAttribute("titulo", "index");
